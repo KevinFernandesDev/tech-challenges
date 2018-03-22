810d31cd5e5fd5e203ee2682022fae40804f0735
Kevin Fernandes <kevin.fernandes.dev@gmail.com>
2018-03-22 18:31:22 +0100

    Implementation of ingame camera done
    - Implemented a base camera class for the behaviour from which to 2 scripts are derivd : one for debugging in the editor and another one when you are in Play mode. 
    - Commenting and refactoring done.
    - The CameraFocus is linked to the DebugCameraFocus so it is easy to test behaviour in editor.
    - For ingame tests, uncheck IsEditorDebugCameraActive to position the camera freely in editor mode before pressing play.
    - [TODO] Change the camera movement animation to have a better Lerp between positions.

---
+++ 3D/senior/iwd-challenge/Assets/Scripts/AbstractCameraFocus.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class AbstractCameraFocus : MonoBehaviour
+{
+    internal Bounds Bounds { get; set; }
+    internal Vector3 TargetPosition { get; set; }
+
+    /// <summary>
+    /// Move the camera over time to the target position defined by an object
+    /// </summary>
+    internal void MoveCamera()
+    {
+        transform.localPosition = Vector3.MoveTowards(transform.position, TargetPosition, 50f * Time.deltaTime);
+    }
+
+    /// <summary>
+    /// Calculate ortographic size of camera from bounds values
+    /// </summary>
+    internal void CalculateOrthographicSizeToFrameTarget(Bounds bounds)
+    {
+        if (!Camera.main.orthographic) return;
+        Camera.main.orthographicSize = Vector3.Distance(bounds.min, bounds.max);
+    }
+
+    /// <summary>
+    /// Calculate the best position for the camera to have the bounds
+    /// object in its view fustrum
+    /// </summary>
+    /// <param name="bounds"></param>
+    internal void CalculateCameraPositionToFrameTarget(Bounds bounds)
+    {
+        // Calculate distance needed to frame the Bounds object in the camera view
+        var distanceFactor = (Vector3.Distance(bounds.min, bounds.max) * 0.5f) / Mathf.Abs(Mathf.Sin(Camera.main.fieldOfView * Mathf.Deg2Rad / 2));
+
+        // Create the target position where the camera should move to have all objects in view
+        Vector3 newTargetPos = bounds.center;
+        TargetPosition = (newTargetPos - (transform.forward * distanceFactor));
+    }
+
+    
+}
---
+++ 3D/senior/iwd-challenge/Assets/Scripts/DebugCameraFocus.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using UnityEngine;
+
+public class DebugCameraFocus : AbstractCameraFocus
+{
+    [SerializeField] private bool IsEditorDebugEnabled = true;
+    [SerializeField] private bool IsEditorDebugCameraActive = true;
+
+    void OnDrawGizmos()
+    {
+        // Only run when game is not in play mode to not impact game performance
+        if (Application.isPlaying) return;
+
+        // Create a new Bounds object combining all bounds from selected scene objects
+        Bounds bounds = Bounds.CreateGroupedBoundsFromRendererList(FindObjectsOfType<MeshRenderer>());
+
+        if (IsEditorDebugEnabled)
+        {
+            // Create vertices position list from the generated Bounds
+            Vector3[] boundsVerticesPositions = bounds.GetVerticesPositions();
+
+            // Draw all gizmos in editor viewport
+            DrawDebugGizmosToViewport(bounds, boundsVerticesPositions);
+        };
+
+        if (!IsEditorDebugCameraActive) return;
+
+        CalculateOrthographicSizeToFrameTarget(bounds);
+        CalculateCameraPositionToFrameTarget(bounds);
+        MoveCamera();
+    }
+
+    private void DrawDebugGizmosToViewport(Bounds bounds, Vector3[] boundsVerticesPositions)
+    {
+        // Draw cube gizmo in viewport representing the calculated Bounds
+        Gizmos.DrawCube(bounds.center, bounds.size);
+
+        // Draw lines pointing from the camera to each of the Bounds vertices
+        Gizmos.color = Color.green;
+        Gizmos.DrawLine(transform.position, boundsVerticesPositions[0]);
+        Gizmos.DrawLine(transform.position, boundsVerticesPositions[1]);
+        Gizmos.DrawLine(transform.position, boundsVerticesPositions[2]);
+        Gizmos.DrawLine(transform.position, boundsVerticesPositions[3]);
+        Gizmos.DrawLine(transform.position, boundsVerticesPositions[4]);
+        Gizmos.DrawLine(transform.position, boundsVerticesPositions[5]);
+        Gizmos.DrawLine(transform.position, boundsVerticesPositions[6]);
+        Gizmos.DrawLine(transform.position, boundsVerticesPositions[7]);
+
+        // Draw red line from the camera to the center of the generated Bounds
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position, bounds.center);
+    }
+}
