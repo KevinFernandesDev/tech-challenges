585fe5cfc6c929e2422cd79f3a2df1fd1d424198
Kevin Fernandes <kevin.fernandes.dev@gmail.com>
2018-03-23 07:51:14 +0100

    Final code review and modification for release
    - Added new movement smoothing to the camera.
    - Added virtual keyword to the AbstractCamera functions to obey SOLID Open-Closed principle on its derived classes.
    - [TODO] Add readme.md to the project.

--- 3D/senior/iwd-challenge/Assets/Scripts/AbstractCameraFocus.cs
+++ 3D/senior/iwd-challenge/Assets/Scripts/AbstractCameraFocus.cs
@@ -4,21 +4,24 @@ using UnityEngine;
 
 public abstract class AbstractCameraFocus : MonoBehaviour
 {
+    public float MovementSmoothing = 25f;
     internal Bounds Bounds { get; set; }
     internal Vector3 TargetPosition { get; set; }
+
+    private Vector3 _currentVelocity = Vector3.zero;
 
     /// <summary>
     /// Move the camera over time to the target position defined by an object
     /// </summary>
-    internal void MoveCamera()
+    internal virtual void MoveCamera()
     {
-        transform.localPosition = Vector3.MoveTowards(transform.position, TargetPosition, 50f * Time.deltaTime);
+        transform.localPosition = Vector3.SmoothDamp(transform.position, TargetPosition, ref _currentVelocity, MovementSmoothing * Time.deltaTime);
     }
 
     /// <summary>
     /// Calculate ortographic size of camera from bounds values
     /// </summary>
-    internal void CalculateOrthographicSizeToFrameTarget(Bounds bounds)
+    internal virtual void CalculateOrthographicSizeToFrameTarget(Bounds bounds)
     {
         if (!Camera.main.orthographic) return;
         Camera.main.orthographicSize = Vector3.Distance(bounds.min, bounds.max);
@@ -29,7 +32,7 @@ public abstract class AbstractCameraFocus : MonoBehaviour
     /// object in its view fustrum
     /// </summary>
     /// <param name="bounds"></param>
-    internal void CalculateCameraPositionToFrameTarget(Bounds bounds)
+    internal virtual void CalculateCameraPositionToFrameTarget(Bounds bounds)
     {
         // Calculate distance needed to frame the Bounds object in the camera view
         var distanceFactor = (Vector3.Distance(bounds.min, bounds.max) * 0.5f) / Mathf.Abs(Mathf.Sin(Camera.main.fieldOfView * Mathf.Deg2Rad / 2));
